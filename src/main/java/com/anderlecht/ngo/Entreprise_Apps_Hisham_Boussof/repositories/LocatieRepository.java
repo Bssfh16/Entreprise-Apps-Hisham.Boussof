db37510bdce34ea7c2ea913e83de3c8c1a87b407
Hisham <boussofhisham@gmail.com>
2026-06-14 15:14:33 +0200

    LocatieRepository aangemaakt en ingevuld

---
+++ src/main/java/com/anderlecht/ngo/Entreprise_Apps_Hisham_Boussof/repositories/LocatieRepository.java
@@ -0,0 +1,7 @@
+package com.anderlecht.ngo.Entreprise_Apps_Hisham_Boussof.repositories;
+
+import com.anderlecht.ngo.Entreprise_Apps_Hisham_Boussof.models.Locatie;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LocatieRepository extends CrudRepository<Locatie, Long> {
+}
